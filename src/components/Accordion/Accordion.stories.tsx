6f60b7e9951b829c540b9b0d8ef4c4e98d3b3694
Komandr33 <and.kom2017@gmail.com>
2024-02-16 19:53:49 +0300

    Add Accordion.stories.tsx

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,50 @@
+import {action} from '@storybook/addon-actions';
+import {Accordion} from './Accordion';
+import React, {useState} from 'react';
+
+export default {
+  title: 'Accordion',
+  component: Accordion,
+};
+
+// type Story = StoryObj<typeof Accordion>;  // новый синтаксис
+//
+// export const FirstStory: Story = {
+//   args: {
+//     title: 'New sintaxis',
+//     collapsed: true,
+//     callbackCollapsed: ()=>{}
+//   },
+// }
+
+const onClickHandler = action('onClick')
+
+export const AccordionCollapsed = () => {
+  return (
+    <Accordion
+      title={'collapsed'}
+      collapsed={true}
+      callbackCollapsed={onClickHandler}/>
+  )
+}
+
+export const AccordionOpen = () => {
+  return (
+    <Accordion
+      title={'open'}
+      collapsed={false}
+      callbackCollapsed={onClickHandler}/>
+  )
+}
+
+export const AccordioDemo = () => {
+
+  const [value, setValue] = useState(true)
+
+  return (
+    <Accordion
+      title={'Demo'}
+      collapsed={value}
+      callbackCollapsed={() => setValue(!value)}/>
+  )
+}
--- src/components/OnOff/IndicatorOnOff.tsx
+++
@@ -1,9 +0,0 @@
-type IndicatorOnOffType = {
-  onOff: boolean
-}
-
-export function IndicatorOnOff(props: IndicatorOnOffType) {
-  return (
-    <span className={props.onOff ? 'indicatorOn' : 'indicatorOff'}></span>
-  );
-}
